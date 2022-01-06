ab98a24024eee2d936ba3e093622ab197a101076
Harshit <srivastavaharshit008@gmail.com>
2022-01-06 17:46:10 +0530

    Swagger config changes

--- src/main/java/com/zensar/MacysOrderServiceApplication.java
+++ src/main/java/com/zensar/MacysOrderServiceApplication.java
@@ -3,14 +3,12 @@ package com.zensar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.zensar.config.SwaggerConfiguration;
 
-@EnableSwagger2
+
+@Import(SwaggerConfiguration.class)
 @EnableAutoConfiguration
 @SpringBootApplication
 public class MacysOrderServiceApplication {
@@ -19,9 +17,4 @@ public class MacysOrderServiceApplication {
 		SpringApplication.run(MacysOrderServiceApplication.class, args);
 	}
 
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.zensar")).build();
-	}
 }
