81d13b15f5b287392e4dc05d64c9b9aab9b36f79
Harshit <srivastavaharshit008@gmail.com>
2022-01-07 13:35:53 +0530

    Added eureka client config to application

--- src/main/java/com/zensar/MacysOrderServiceApplication.java
+++ src/main/java/com/zensar/MacysOrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.zensar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 import com.zensar.config.SwaggerConfiguration;
@@ -11,6 +12,7 @@ import com.zensar.config.SwaggerConfiguration;
 @Import(SwaggerConfiguration.class)
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableEurekaClient
 public class MacysOrderServiceApplication {
 
 	public static void main(String[] args) {
